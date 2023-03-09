44215db3c4219d505eddc170a8cb7fe5ae9fd842
zanedr <zanedanielrussell@gmail.com>
2023-03-09 15:28:48 -0700

    update module declaration

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,7 @@
 declare class SigningStargateClientWithLit {
-  static createClient(pubKey: any, authSig: any, rpc: any): Promise<SigningStargateClientWithLit>;
+  static createClient(pubKey: string, authSig: any, rpc: any): Promise<SigningStargateClientWithLit>;
 
-  sendTokens(recipient: any, amount: any, fee: any): Promise<any>;
+  sendTokens(recipient: string, amount: any, fee: any): Promise<any>;
 }
 
 export = SigningStargateClientWithLit;
